badf08ef888761c07abe71a6dc4731b3173e305e
danitza18 <danitzamarlenechurasocujayo@gmail.com>
2019-09-14 23:16:58 -0500

    mi primer capa web con MVC

---
+++ src/test/java/almacen_para_cualquier_empresa/tarea_almacen/TareaAlmacenApplicationTests.java
@@ -0,0 +1,16 @@
+package almacen_para_cualquier_empresa.tarea_almacen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TareaAlmacenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
